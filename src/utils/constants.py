34ed4554b77d72b2f88ef5bf184c4be2727d8a2a
Christos Natsis <christos.natsis@incelligent.net>
2021-06-26 18:51:12 +0300

    added env file

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,3 +6,9 @@ env.read_env()
 
 PRODUCER_INTERVAL = env.int('PRODUCER_INTERVAL', default=5)
 SITES_FILE_PATH = os.path.join(os.path.dirname(__file__), '../../conf/sites.txt')
+
+AIVEN_TOKEN = env.str('AIVEN_TOKEN')
+AIVEN_PROJECT = env.str('AIVEN_PROJECT', default='sites-availability')
+AIVEN_API_URL = env.str('AIVEN_API_URL')
+AIVEN_KAFKA_NAME = env.str('AIVEN_KAFKA_NAME')
+AIVEN_PG_NAME = env.str('AIVEN_PG_NAME')
